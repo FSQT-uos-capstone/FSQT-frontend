8daba42257a0e03be1babc2046ce04515a5ea2e3
r4k0nb4k0n <chj6097@gmail.com>
2021-03-14 17:31:46 +0900

    (issue-2) /gate 레이아웃 및 페이지 추가

---
+++ src/layouts/GateLayout.vue
@@ -0,0 +1,59 @@
+<template>
+  <q-layout class="GateLayout" view="hHh lpR fFf">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer class="Footer row">
+      <q-toolbar class="Toolbar justify-evenly">
+        <q-btn unelevated :ripple="false" class="LoginButton" label="로그인" />
+        <q-btn
+          unelevated
+          :ripple="false"
+          class="RegisterButton"
+          label="집사 등록하기"
+        />
+      </q-toolbar>
+    </q-footer>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: "GateLayout",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss">
+.GateLayout {
+  .Footer {
+    background-color: #ffffff;
+    color: #000000;
+    height: 10vh;
+  }
+  .Toolbar {
+    height: 100%;
+    .LoginButton {
+      width: 47.5%;
+      height: 65%;
+      border: 2px solid #000000;
+      border-radius: 6px;
+      font-size: 0.9rem;
+      font-weight: bold;
+    }
+    .RegisterButton {
+      width: 47.5%;
+      height: 65%;
+      border: 2px solid #000000;
+      border-radius: 6px;
+      font-size: 0.9rem;
+      font-weight: bold;
+      background: #000000;
+      color: #ffffff;
+    }
+  }
+}
+</style>
