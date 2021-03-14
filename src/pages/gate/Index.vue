ef9c78b13f74c8b397d764179a0b0dbd7e469920
Choe Hyeong Jin <chj6097@gmail.com>
2021-03-14 17:45:20 +0900

    Issue 2 (#13)
    * (issue-2) 필요한 글꼴 추가
    
    * (issue-2) 기본 글꼴 설정
    
    * (issue-2) /gate 레이아웃 및 페이지 추가
    
    * (issue-2) /gate 리다이렉트 임시 조치

---
+++ src/pages/gate/Index.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-page class="flex flex-center PageGateIndex">
+    <div class="Title">
+      냥중일기
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "PageGateIndex"
+};
+</script>
+
+<style lang="scss">
+.PageGateIndex {
+  background-color: #000000;
+  .Title {
+    writing-mode: vertical-lr;
+    text-orientation: upright;
+    color: #ffffff;
+    font-family: "NanumMyeongjo-Regular";
+    font-size: 4rem;
+    padding-top: 16px;
+    padding-bottom: 16px;
+    padding-left: 12px;
+    padding-right: 12px;
+    border: 1px solid #ffffff;
+  }
+}
+</style>
