49caf85f749764fe521e548ba7cedc2688a63f26
r4k0nb4k0n <chj6097@gmail.com>
2021-03-17 00:02:25 +0900

    (issue-3) /auth 레이아웃 추가

---
+++ src/layouts/AuthLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-layout class="AuthLayout" view="hHh lpR fFf">
+    <q-toolbar class="Toolbar">
+      <q-btn v-go-back flat round color="black" icon="west" />
+    </q-toolbar>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: "AuthLayout",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss">
+.AuthLayout {
+  background: #ffffff;
+  .Toolbar {
+    padding: 0;
+  }
+}
+</style>
