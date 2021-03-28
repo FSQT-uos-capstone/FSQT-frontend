a05d7d98b7b6161395ab65ecb88f867fa5360506
r4k0nb4k0n <chj6097@gmail.com>
2021-03-28 19:01:27 +0900

    Implement Discovery Page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,30 +1,49 @@
 <template>
-  <q-page class="flex flex-center">
-    <div style="width: 100vw">
-      <Diary
-        v-for="content in contents"
-        :key="content.id"
-        :cat="content.cat"
-        :user="content.user"
-        :date="content.date"
-        :weather="content.weather"
-        :mood="content.mood"
-        :photoUrl="content.photoUrl"
-        :diaryContent="content.diaryContent"
-        :likes="content.likes"
-        :comment="content.comment"
-      ></Diary>
+  <q-page class="column flex PageDiscoverIndex">
+    <q-space />
+    <div class="row Title">
+      고양이 일기장
+    </div>
+    <q-space />
+    <div class="row Content">
+      <carousel
+        :paginationEnabled="false"
+        :perPage="1"
+        :scrollPerPage="true"
+        style="max-width: 100vw"
+      >
+        <slide
+          v-for="content in contents"
+          :key="content.id"
+          style="max-width: 100vw; width: 100vw;"
+        >
+          <Diary
+            :cat="content.cat"
+            :user="content.user"
+            :date="content.date"
+            :weather="content.weather"
+            :mood="content.mood"
+            :photoUrl="content.photoUrl"
+            :diaryContent="content.diaryContent"
+            :likes="content.likes"
+            :comment="content.comment"
+          ></Diary>
+        </slide>
+      </carousel>
     </div>
   </q-page>
 </template>
 
 <script>
 import Diary from "../components/Diary";
+import { Carousel, Slide } from "vue-carousel";
 
 export default {
   name: "PageIndex",
   components: {
-    Diary
+    Diary,
+    Carousel,
+    Slide
   },
   data() {
     return {
@@ -50,9 +69,93 @@ export default {
             "난 차가운 도시의 고양이...\n오늘도 밥을 먹는다.\n오늘은 사료 맛이 괜찮구만.",
           likes: 114,
           comment: 35
+        },
+        {
+          id: 1,
+          cat: {
+            id: 0,
+            name: "시냥이1",
+            profileUrl: "https://cdn.quasar.dev/img/avatar2.jpg",
+            location: "시립대 정문"
+          },
+          user: {
+            id: 0,
+            name: "시냥집사1",
+            profileUrl: "https://cdn.quasar.dev/img/avatar4.jpg"
+          },
+          date: "2021년 4월 2일 뭔요일",
+          weather: "☁︎",
+          mood: "😎",
+          photoUrl: "https://placeimg.com/1000/1000/any",
+          diaryContent:
+            "난 차가운 도시의 고양이...\n오늘도 밥을 먹는다.\n오늘은 사료 맛이 괜찮구만.",
+          likes: 114,
+          comment: 35
+        },
+        {
+          id: 2,
+          cat: {
+            id: 0,
+            name: "시냥이1",
+            profileUrl: "https://cdn.quasar.dev/img/avatar2.jpg",
+            location: "시립대 정문"
+          },
+          user: {
+            id: 0,
+            name: "시냥집사1",
+            profileUrl: "https://cdn.quasar.dev/img/avatar4.jpg"
+          },
+          date: "2021년 4월 2일 뭔요일",
+          weather: "☁︎",
+          mood: "😎",
+          photoUrl: "https://placeimg.com/1000/1000/any",
+          diaryContent:
+            "난 차가운 도시의 고양이...\n오늘도 밥을 먹는다.\n오늘은 사료 맛이 괜찮구만.",
+          likes: 114,
+          comment: 35
+        },
+        {
+          id: 3,
+          cat: {
+            id: 0,
+            name: "시냥이1",
+            profileUrl: "https://cdn.quasar.dev/img/avatar2.jpg",
+            location: "시립대 정문"
+          },
+          user: {
+            id: 0,
+            name: "시냥집사1",
+            profileUrl: "https://cdn.quasar.dev/img/avatar4.jpg"
+          },
+          date: "2021년 4월 2일 뭔요일",
+          weather: "☁︎",
+          mood: "😎",
+          photoUrl: "https://placeimg.com/1000/1000/any",
+          diaryContent:
+            "난 차가운 도시의 고양이...\n오늘도 밥을 먹는다.\n오늘은 사료 맛이 괜찮구만.",
+          likes: 114,
+          comment: 35
         }
       ]
     };
   }
 };
 </script>
+
+<style lang="scss">
+.PageDiscoverIndex {
+  .Toolbar {
+    padding: 0;
+  }
+  .Title {
+    color: #000000;
+    font-size: 2.27rem;
+    font-weight: medium;
+    padding-left: 20px;
+  }
+  .Content {
+    max-width: 100vw;
+    width: 100vw;
+  }
+}
+</style>
