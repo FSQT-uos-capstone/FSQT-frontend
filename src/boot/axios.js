2cd935b054cc4cda9e36cd0eb8149e85f45b194b
r4k0nb4k0n <chj6097@gmail.com>
2021-05-06 19:11:07 +0900

    Update axios boot file

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,4 +1,14 @@
-import Vue from 'vue'
-import axios from 'axios'
+import Vue from "vue";
+import axios from "axios";
 
-Vue.prototype.$axios = axios
+Vue.prototype.$axios = axios;
+
+const api = axios.create({ baseURL: "http://3.36.71.58" });
+const chatbot = axios.create({
+  baseURL: "https://catchat-ftlwa.run.goorm.io/api/chat"
+});
+
+Vue.prototype.$api = api;
+Vue.prototype.$chatbot = chatbot;
+
+export { axios, api, chatbot };
