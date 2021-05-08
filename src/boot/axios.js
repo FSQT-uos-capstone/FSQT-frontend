d4e25948296335e17162fbd6ab73bb9958f89e9b
r4k0nb4k0n <chj6097@gmail.com>
2021-05-08 22:42:31 +0900

    API 고정 IP로 수정

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default ({ store, Vue }) => {
   Vue.prototype.$axios = axios;
 
-  const api = axios.create({ baseURL: "http://3.35.52.89" });
+  const api = axios.create({ baseURL: "http://3.36.179.76" });
   const token = store.getters["auth/token"];
   if (token) {
     api.defaults.headers.common["Authorization"] = token;
