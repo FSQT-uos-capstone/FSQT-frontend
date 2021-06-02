d61ed19fb87a1e78e0b63ebe90f5f7863792a50d
r4k0nb4k0n <chj6097@gmail.com>
2021-06-03 05:26:31 +0900

    유저 프로필 UI 걔선

--- src/pages/users/Profile.vue
+++ src/pages/users/Profile.vue
@@ -1,174 +1,173 @@
 <template>
-  <q-page class="column Profile">
-    <div class="column" style="width: 100vw">
-      <div class="flex justify-center">
-        <q-dialog
-          v-model="uploader"
-          persistent
-          maximized
-          transition-show="slide-up"
-          transition-hide="slide-down"
-        >
-          <q-card class="my-card">
-            <q-card-section class="row items-center">
-              <q-btn
-                icon="eva-close"
-                flat
-                round
-                dense
-                v-close-popup
-                @click="closeDialog"
-              />
-            </q-card-section>
-            <q-stepper
-              ref="stepper"
-              v-model="step"
-              vertical
-              color="black"
-              active-color="pink-13"
-              done-color="black"
-              animated
-              keep-alive
-            >
-              <q-step
-                :name="1"
-                title="프로필 사진 고르기"
-                icon="eva-image-outline"
-                :done="step > 1"
-              >
-                Drag, pinch and zoom으로 올라갈 사진을 간단하게 바꿔보세요!
-
-                <croppa
-                  v-model="myCroppa"
-                  placeholder="프로필 사진을 선택해주세요"
-                  placeholder-color="#000"
-                  canvas-color="transparent"
-                  :show-loading="true"
-                ></croppa>
-
-                <q-stepper-navigation>
-                  <q-btn
-                    unelevated
-                    @click="selectFileAndUploadProfilePhoto"
-                    color="black"
-                    label="다음"
-                    :disable="myCroppa.chosenFile === null"
-                  />
-                </q-stepper-navigation>
-              </q-step>
-              <q-step
-                :name="2"
-                title="업로드 완료!"
-                icon="eva-checkmark-circle-outline"
-              >
-                집사의 프로필 사진이 바뀌었습니다. 고양이와 다른 집사들에게 좋은
-                인상을 남길 수 있을 거에요!
-                <q-stepper-navigation>
-                  <q-btn
-                    unelevated
-                    @click="closeDialogWhenComplete"
-                    color="black"
-                    label="완료"
-                  />
-                </q-stepper-navigation>
-              </q-step>
-            </q-stepper>
-          </q-card>
-        </q-dialog>
-        <q-avatar
-          v-if="userTarget.profile_img === null"
-          size="6rem"
-          color="purple"
-          text-color="white"
-          class="q-my-md"
-        >
-          {{ userTarget.nickname ? userTarget.nickname[0] : "" }}
-          <q-badge
-            v-if="authUserId === parseInt(userId)"
-            floating
-            rounded
-            color="grey"
-            @click="uploader = true"
-          >
-            <q-icon
-              color="white"
-              name="eva-edit-outline"
-              style="font-size: 1.3rem"
-            ></q-icon>
-          </q-badge>
-        </q-avatar>
-        <q-avatar v-else size="6rem" class="q-my-md">
-          <img :src="userTarget.profile_img" />
-          <q-badge
-            v-if="authUserId === parseInt(userId)"
-            floating
-            rounded
-            color="grey"
-            @click="uploader = true"
-          >
-            <q-icon
-              color="white"
-              name="eva-edit-outline"
-              style="font-size: 1.3rem"
-            ></q-icon>
-          </q-badge>
-        </q-avatar>
-      </div>
-      <div class="text-h5 text-center text-weight-bold">
-        {{ userTarget.nickname ? userTarget.nickname : "" }}
-      </div>
-      <div class="flex justify-evenly q-my-md">
-        <!--<q-btn unelevated :ripple="false" class="FollowButton" label="모시기" />-->
-      </div>
+  <q-page class="column flex Profile">
+    <div class="flex justify-center">
       <q-dialog
-        v-model="showSelectedDiary"
-        transition-show="scale"
-        transition-hide="scale"
-        @before-hide="unselectDiary"
+        v-model="uploader"
+        persistent
+        maximized
+        transition-show="slide-up"
+        transition-hide="slide-down"
       >
-        <q-card
-          style="padding: 10px; min-width: calc(100vw - 40px); max-width: calc(100vw - 40px); width: calc(100vw - 40px)"
-        >
-          <Diary
-            v-if="selectedDiary.id === -1"
-            :id="selectedDiary.id"
-            :cat="selectedDiary.cat"
-            :user="selectedDiary.user"
-            :profile="selectedDiary.profile"
-            :date="selectedDiary.date"
-            :dayOfTheWeek="selectedDiary.dayOfTheWeek"
-            :photoUrl="selectedDiary.photoURL"
-            :diaryContent="selectedDiary.content"
-            :emotions="selectedDiary.emotions"
-            :tags="selectedDiary.tags"
-          ></Diary>
-          <Diary
-            v-else
-            :id="diaries[selectedDiary.idx].id"
-            :cat="diaries[selectedDiary.idx].cat"
-            :user="diaries[selectedDiary.idx].user"
-            :profile="diaries[selectedDiary.idx].profile"
-            :date="diaries[selectedDiary.idx].date"
-            :dayOfTheWeek="diaries[selectedDiary.idx].dayOfTheWeek"
-            :photoUrl="diaries[selectedDiary.idx].photoURL"
-            :diaryContent="diaries[selectedDiary.idx].content"
-            :emotions="diaries[selectedDiary.idx].emotions"
-            :tags="diaries[selectedDiary.idx].tags"
-          ></Diary>
+        <q-card class="my-card">
+          <q-card-section class="row items-center">
+            <q-btn
+              icon="eva-close"
+              flat
+              round
+              dense
+              v-close-popup
+              @click="closeDialog"
+            />
+          </q-card-section>
+          <q-stepper
+            ref="stepper"
+            v-model="step"
+            vertical
+            color="black"
+            active-color="pink-13"
+            done-color="black"
+            animated
+            keep-alive
+          >
+            <q-step
+              :name="1"
+              title="프로필 사진 고르기"
+              icon="eva-image-outline"
+              :done="step > 1"
+            >
+              Drag, pinch and zoom으로 올라갈 사진을 간단하게 바꿔보세요!
+
+              <croppa
+                v-model="myCroppa"
+                placeholder="프로필 사진을 선택해주세요"
+                placeholder-color="#000"
+                canvas-color="transparent"
+                :show-loading="true"
+              ></croppa>
+
+              <q-stepper-navigation>
+                <q-btn
+                  unelevated
+                  @click="selectFileAndUploadProfilePhoto"
+                  color="black"
+                  label="다음"
+                  :disable="myCroppa.chosenFile === null"
+                />
+              </q-stepper-navigation>
+            </q-step>
+            <q-step
+              :name="2"
+              title="업로드 완료!"
+              icon="eva-checkmark-circle-outline"
+            >
+              집사의 프로필 사진이 바뀌었습니다. 고양이와 다른 집사들에게 좋은
+              인상을 남길 수 있을 거에요!
+              <q-stepper-navigation>
+                <q-btn
+                  unelevated
+                  @click="closeDialogWhenComplete"
+                  color="black"
+                  label="완료"
+                />
+              </q-stepper-navigation>
+            </q-step>
+          </q-stepper>
         </q-card>
       </q-dialog>
-      <div class="flex justify-evenly wrap q-mx-sm">
-        <q-img
-          v-for="(diary, idx) in diaries"
-          :src="diary.photoURL"
-          width="45%"
-          class="q-mb-md"
-          :ratio="1"
-          :key="diary.id"
-          @click="selectDiary(diary, idx)"
-        />
-      </div>
+      <q-avatar
+        v-if="userTarget.profile_img === null"
+        size="6rem"
+        color="purple"
+        text-color="white"
+        class="q-my-md"
+      >
+        {{ userTarget.nickname ? userTarget.nickname[0] : "" }}
+        <q-badge
+          v-if="authUserId === parseInt(userId)"
+          floating
+          rounded
+          color="grey"
+          @click="uploader = true"
+        >
+          <q-icon
+            color="white"
+            name="eva-edit-outline"
+            style="font-size: 1.3rem"
+          ></q-icon>
+        </q-badge>
+      </q-avatar>
+      <q-avatar v-else size="6rem" class="q-my-md">
+        <img :src="userTarget.profile_img" />
+        <q-badge
+          v-if="authUserId === parseInt(userId)"
+          floating
+          rounded
+          color="grey"
+          @click="uploader = true"
+        >
+          <q-icon
+            color="white"
+            name="eva-edit-outline"
+            style="font-size: 1.3rem"
+          ></q-icon>
+        </q-badge>
+      </q-avatar>
     </div>
+    <div class="text-h5 text-center text-weight-bold">
+      {{ userTarget.nickname ? userTarget.nickname : "" }}
+    </div>
+    <q-dialog
+      v-model="showSelectedDiary"
+      transition-show="scale"
+      transition-hide="scale"
+      @before-hide="unselectDiary"
+    >
+      <q-card
+        style="padding: 10px; min-width: calc(100vw - 40px); max-width: calc(100vw - 40px); width: calc(100vw - 40px)"
+      >
+        <Diary
+          v-if="selectedDiary.id === -1"
+          :id="selectedDiary.id"
+          :cat="selectedDiary.cat"
+          :user="selectedDiary.user"
+          :profile="selectedDiary.profile"
+          :date="selectedDiary.date"
+          :dayOfTheWeek="selectedDiary.dayOfTheWeek"
+          :photoUrl="selectedDiary.photoURL"
+          :diaryContent="selectedDiary.content"
+          :emotions="selectedDiary.emotions"
+          :tags="selectedDiary.tags"
+        ></Diary>
+        <Diary
+          v-else
+          :id="diaries[selectedDiary.idx].id"
+          :cat="diaries[selectedDiary.idx].cat"
+          :user="diaries[selectedDiary.idx].user"
+          :profile="diaries[selectedDiary.idx].profile"
+          :date="diaries[selectedDiary.idx].date"
+          :dayOfTheWeek="diaries[selectedDiary.idx].dayOfTheWeek"
+          :photoUrl="diaries[selectedDiary.idx].photoURL"
+          :diaryContent="diaries[selectedDiary.idx].content"
+          :emotions="diaries[selectedDiary.idx].emotions"
+          :tags="diaries[selectedDiary.idx].tags"
+        ></Diary>
+      </q-card>
+    </q-dialog>
+    <div
+      class="fit row wrap justify-between"
+      style="width: calc(100vw - 40px); max-width: calc(100vw - 40px); min-width: calc(100vw - 40px); margin-top: 20px; margin-left: 20px; margin-right: 20px"
+    >
+      <q-img
+        v-for="(diary, idx) in diaries"
+        :src="diary.photoURL"
+        width="47.5%"
+        class="q-mb-md"
+        :ratio="1"
+        :key="diary.id"
+        @click="selectDiary(diary, idx)"
+      />
+    </div>
+    <q-space></q-space>
   </q-page>
 </template>
 
